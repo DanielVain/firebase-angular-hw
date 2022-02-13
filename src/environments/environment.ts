4c88b47290d430ed6ee4f1f202e9e9b3e2bc07a4
Daniel Vainerman <danielvain16@gmail.com>
2022-02-13 16:55:34 +0200

    added firebase sdk

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCIYb0GYQ6UeIq9nWeqVeyNSp9WKyqJfl8',
+    authDomain: 'fir-angular-hw.firebaseapp.com',
+    databaseURL: 'https://fir-angular-hw-default-rtdb.firebaseio.com',
+    projectId: 'fir-angular-hw',
+    storageBucket: 'fir-angular-hw.appspot.com',
+    messagingSenderId: '754988265941',
+    appId: '1:754988265941:web:13d27f74aa2b97287e0d5c',
+  },
 };
 
 /*
