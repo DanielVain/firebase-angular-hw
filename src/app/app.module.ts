6defd0e047a18dd9061b87cf8e079c24a80f7d83
Daniel Vainerman <danielvain16@gmail.com>
2022-02-13 16:50:52 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SignupPageComponent,
+    LoginPageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
