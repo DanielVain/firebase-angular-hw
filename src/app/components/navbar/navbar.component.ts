4be2c8f8b5ef35d2709996b0c962cd2983d15f61
Daniel Vainerman <danielvain16@gmail.com>
2022-02-15 11:44:39 +0200

    onDestroy change

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -7,17 +8,22 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
   links: string[];
   isLoggedIn: boolean;
+  loggedInChangeSub: Subscription;
   constructor(private authService: AuthService, private router: Router) {
     this.links = ['home', 'dashboard'];
   }
-
+  ngOnDestroy(): void {
+    this.loggedInChangeSub.unsubscribe();
+  }
   ngOnInit(): void {
-    this.authService.logChange.subscribe((status) => {
-      this.isLoggedIn = status;
-    });
+    this.loggedInChangeSub = this.authService.logChange.subscribe(
+      (status: boolean) => {
+        this.isLoggedIn = status;
+      }
+    );
   }
   handleLogout(): void {
     this.authService.logout();
